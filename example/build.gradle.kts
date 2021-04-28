9a08107b8e8b55645e7f8947ee576c640909e166
Martijn Dwars <martijn.dwars@oracle.com>
2021-04-28 11:27:51 +0200

    Add PGX Algorithm program

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -4,14 +4,16 @@ plugins {
 
 repositories {
   jcenter()
+  maven {
+    url = uri("https://artifacthub-phx.oci.oraclecorp.com/labs-pgx-release-local/")
+  }
 }
 
 dependencies {
   compileOnly(project(":plugin"))
+  compileOnly("oracle.gm.lang:algorithm.api:0.1.2-20210305")
 }
 
 tasks.withType<JavaCompile> {
   options.compilerArgs.add("-Xplugin:SamplePlugin")
 }
-
-// Add 
--- example/src/main/java/Test.java
+++ example/src/main/java/Test.java
@@ -1,3 +1,12 @@
+import oracle.pgx.algorithm.PgxGraph;
+import oracle.pgx.algorithm.annotations.GraphAlgorithm;
+import oracle.pgx.algorithm.annotations.Out;
+import oracle.pgx.algorithm.VertexProperty;
+import oracle.pgx.algorithm.VertexSet;
+
+@GraphAlgorithm
 public class Test {
-  foo x;
+  public void markNeighbours(PgxGraph g, int hop, @Out VertexProperty<Boolean> mark) {
+    int currentHop = 0;
+  }
 }
